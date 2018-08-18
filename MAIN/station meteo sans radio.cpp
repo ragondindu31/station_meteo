7f71714d7f4374f23e2d67a025f66b171b7d9d97
Ragondin31 <regis.chollat@gmail.com>
2018-08-18 17:41:13 +0200

    map appui BP
    mesure T° air, humidité de l'air et du sol,
    affichage différent suivant nbre appuis sur BP

--- MAIN/station meteo sans radio.cpp
+++ MAIN/station meteo sans radio.cpp
@@ -4,7 +4,7 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 
-/*branchement
+/*branchements :
 ecran:
 RS :9
 E : 10
@@ -21,21 +21,31 @@ SCl : A5
 
 bouton poussoir : 
 D2
+
+humidité du sol :
+A0 : S
+GND 
++5v
+
 */
 volatile bool interruption =0;
 int bouton=0;//test pour appui
+
+int sensorPin = A0; // sélection de la pin de mesure du capteur d'humidité de sol
+int sensorValue = 0; // initialisation de la valeur du capteur
+int seuil_alerte = 300; // valeur du seuil à partir duquel l'alerte est donnée
+int NbreAppuis=0; //variable pour modifer l'affichage
 
 // Set dht12 i2c comunication on default Wire pin
 DHT12 dht12;
 
 LiquidCrystal monEcran(9,10,5,4,3,6); // on crée l'objet écran11,12 >9,8
 
-/*fonction qui sert à signaler interruption bouton*/ 		
+/*fonction effectuer pendant interruption (appui sur bouton) */ 		
 
 void appuiBouton (void) {
 	
-	
-	interruption =1;		    
+	interruption = HIGH;
 }
 
 void sleepNow (){
@@ -48,12 +58,81 @@ void sleepNow (){
 	detachInterrupt(bouton);	
 }
 
+void ReadWriteTempeHeAir (){
+/*mesure et affichage de la temprature et de l'humidité*/
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+	// Read temperature as Celsius (the default)
+	float t12 = dht12.readTemperature();
+	
+	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+	float h12 = dht12.readHumidity();
+
+	bool dht12Read = true;
+	// Check if any reads failed and exit early (to try again).
+	if (isnan(h12) || isnan(t12) )
+	{
+		monEcran.clear(); // on efface l'écran
+		monEcran.print("Failed to read from DHT12 sensor!");
+
+		dht12Read = false;
+	}
+
+	if (dht12Read)
+	{
+		monEcran.clear();	
+		monEcran.setCursor(0,0);
+		monEcran.print("temp: ");
+		monEcran.print(int(t12));
+		monEcran.print(".");
+    	monEcran.print((int)((float)(t12 - (int)t12) * 10)); // affiche temperature
+
+    	monEcran.setCursor(0,1);
+    	monEcran.print("humidite : ");
+    	monEcran.print(int (h12));
+    	monEcran.print(".");
+    	monEcran.print((int)((float)(h12 - (int)h12) * 10)); // affiche temperature
+	}
+}
+
+void ReadWriteHeSol (){
+	//lecture de l'H°Sol
+	sensorValue = analogRead(sensorPin); // lecture directe de la valeur
+	monEcran.clear();	
+	monEcran.setCursor(0,0);
+	monEcran.print("humidite du sol : ");
+	monEcran.setCursor(0,1);
+	monEcran.print(sensorValue);
+}
+
+void affichage() {
+	/*
+	apres le premier appui sur bouton affichage des données T° et humidité de l'air (H°Air)
+	2° appui : affichage Humidité du Sol (H°Sol)
+	*/
+	switch (NbreAppuis) {
+		
+		case 1 :
+			ReadWriteTempeHeAir();
+			break;
+		case 2 :
+			ReadWriteHeSol();
+			NbreAppuis= 0;
+			break;
+		default :
+			monEcran.clear();
+			monEcran.setCursor(0,0);
+			monEcran.print("probleme");
+			delay (2000);
+			break;
+	}
+}
 void setup() {
 
+	//initialisation de l'écran
 	monEcran.begin(16,2); //on initialise la communication avec 16 colonnes et deux lignes
 	monEcran.clear(); // on efface l'écran
-	monEcran.print("bonjour"); // on écrit Bonjour
-
+	
 	// Start sensor handshake
 	dht12.begin();
 	
@@ -61,53 +140,20 @@ void setup() {
 
 
 void loop() {
+	//activation des interruptions pour detection de appuis sur BP
+	pinMode(bouton, INPUT_PULLUP);
+	attachInterrupt(bouton, appuiBouton , FALLING);//parametrage interruption
+	delay (50);
 
-	
-
-	if (interruption == 1) {
-	
-	monEcran.print("Bonjour inconnu");
-	delay (1500);
-	/*mesure et affichage de la temprature et de l'humidité*/
-
-        // Reading temperature or humidity takes about 250 milliseconds!
-		// Read temperature as Celsius (the default)
-		float t12 = dht12.readTemperature();
-		
-		// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-		float h12 = dht12.readHumidity();
-
-		bool dht12Read = true;
-		// Check if any reads failed and exit early (to try again).
-		if (isnan(h12) || isnan(t12) )
-		{
-			monEcran.clear(); // on efface l'écran
-			monEcran.print("Failed to read from DHT12 sensor!");
-
-			dht12Read = false;
-		}
-
-		if (dht12Read)
-		{
-			monEcran.clear();	
-			monEcran.setCursor(0,0);
-			monEcran.print("temp: ");
-			monEcran.print(int(t12));
-			monEcran.print(".");
-	    	monEcran.print((int)((float)(t12 - (int)t12) * 10)); // affiche temperature
-
-	    	monEcran.setCursor(0,1);
-	    	monEcran.print("humidite : ");
-	    	monEcran.print(int (h12));
-	    	monEcran.print(".");
-	    	monEcran.print((int)((float)(h12 - (int)h12) * 10)); // affiche temperature
-		}
-
-		delay (3000);
-		interruption=0;
-		monEcran.clear();
-
+	if (interruption!=0) {
+		NbreAppuis++;
 	}
+
+	if (NbreAppuis!= 0) {
+		affichage();
+	}
+	delay (2000);
+	monEcran.clear();
 	sleepNow(); 
 }
 
