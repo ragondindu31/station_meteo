723aa509631281a17475f8f8147adb98b70cb0f0
ragondindu31 <35045736+ragondindu31@users.noreply.github.com>
2019-09-11 21:43:44 +0200

    début de mise au point de l'écran OLED et rangement des fichiers

--- MAIN/station_meteo_radio_rtc_sleep_huAirSol/station_meteo_EmetteurRadio_rtc_sleep_huAirSol.cpp
+++ MAIN/station_meteo_radio_rtc_sleep_huAirSol/station_meteo_EmetteurRadio_rtc_sleep_huAirSol.cpp
@@ -14,18 +14,13 @@
 mesure humidite air sol et temperature
 mode sleep
 rtc reveil toute les min et 5s
-Affichage sur liaison serie toute les min et 5s
 emission radio 
-je suis pas sur qu'on puisse faire de la com avec plusieurs modules mais on verra plus tard
-
-16/05 emission pas au point, neccessité de réalise recepteur sur avant de faire mise au point emetteur
-03/06 emission simple j'en ai marre
-        emission ok mais pas la facon de creer un message PROBLEME AFFICHAGE SUR RECEVEUR DE LA TEMPERATURE
-        ecriture de float envoi de int reception int et traitement de float prbm!!!!
-04/06        tableau de float en envoi et en reception. un float ok puis 000000. tenté d'envoyer d'autre caractere en premier
 09/06 enfin j'envoie et je receptionne ce que je veux
 emission toute les minute d'un message selon la description du tableur "def trames com radio"
 reception utiliser soft NRF24recepteurV3.ino
+créer des fonctions pour emission des différents messages meme si pas utilisé encore
+faire une mesure toute les 10 minutes et une envoi pour le moment
+nettoyer tout les fichiers
 */
 
 /*branchements :
@@ -74,8 +69,6 @@ RF24 radio(9, 10); // CE, CSN
 
 
 //constantes
-//const byte address[6] = "00001";
-
 const uint8_t SQW_PIN(2);   // connect this pin to DS3231 INT/SQW pin
 const char text[] = "humidité du sol : ";
 
@@ -84,9 +77,6 @@ const char text[] = "humidité du sol : ";
 //variables
 volatile boolean alarmIsrWasCalled = false;
 
-//int HSol = 0; // initialisation de la valeur du capteur //transformation en char d'int 
-//float t12=0;
-//float h12=0;
 struct data{
     char idmodule;// numéro de module en prevision
     char typemsg;// défini quel type de message est envoyé par le module
@@ -94,6 +84,7 @@ struct data{
     int HSol;
     float t12; 
     float h12;
+    ;
 };
 struct data msgradio;
 
@@ -190,27 +181,56 @@ void sleepNow (void){
 	sleep_mode();//mise en veille
 }
 
-//parametrage des interruption de la RTC
+//parametrage des interruptions de la RTC
 void parametrageRTC (void) {
     pinMode(SQW_PIN, INPUT_PULLUP);
-    // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
-    RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
-    RTC.alarm(ALARM_1);
-    RTC.alarmInterrupt(ALARM_1, false);
-    RTC.squareWave(SQWAVE_NONE);
-    
     // set Alarm 1 to occur at 5 seconds after every minute
-    RTC.setAlarm(ALM1_MATCH_SECONDS, 5, 0, 0, 1);
+    //RTC.setAlarm(ALM1_MATCH_SECONDS, 5, 0, 0, 1);
+
+    // set Alarm 1 to occur at 5 minuts all hours
+    //RTC.setAlarm(ALM1_MATCH_MINUTES,2, 05,0,1);
+
+    // set Alarm 1 to occur once per second
+    RTC.setAlarm(ALM1_EVERY_SECOND, 0, 0, 0, 0);
+
     // clear the alarm flag
     RTC.alarm(ALARM_1);
     RTC.alarmInterrupt(ALARM_1, true);
     attachInterrupt(INT0, AwAke, FALLING);
 }
 
+void parametrageradio(void) {
+    radio.begin();
+    radio.setAutoAck(1);                    // Ensure autoACK is enabled
+    radio.enableAckPayload();               // Allow optional ack payloads
+    radio.setRetries(5,15);                 // Smallest time between retries, max no. of retries
+    radio.setPayloadSize(32);                // Here we are sending 1-byte payloads to test the call-response speed
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+   
+    radio.stopListening();
+    //     radio.printDetails();
+}
+
+void envoimesure(struct data){
+
+++msgradio.nummsg;
+msgradio.typemsg=1;
+/*debug
+Serial.print("numéro du msg : ");
+Serial.println(int(msgradio.nummsg));
+*/
+    if (!radio.write( &msgradio, sizeof(msgradio) ))
+    {
+       Serial.println(F("failed"));
+    }  
+
+
+}
 
 void setup() {
 	
-    msgradio.idmodule=1;
+    
 	
     /*DEBUG*/
     Serial.begin(112560);
@@ -227,20 +247,11 @@ void setup() {
     //parametrage des interruption de la RTC
     parametrageRTC(); //pour debug
     
-    /*parametrage radio*/
-    radio.begin();
-    radio.setAutoAck(1);                    // Ensure autoACK is enabled
-    radio.enableAckPayload();               // Allow optional ack payloads
-    radio.setRetries(5,15);                 // Smallest time between retries, max no. of retries
-    radio.setPayloadSize(32);                // Here we are sending 1-byte payloads to test the call-response speed
-    //radio.openWritingPipe(address);        // Both radios listen on the same pipes by default, and switch when writing
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-   
-    radio.stopListening();  
-    //radio.startListening();
-    // Dump the configuration of the rf unit for debugging
-    radio.printDetails();
+    //parametrage radio
+    parametrageradio();
+
+    //paramétrage de l'ID du module radio
+    msgradio.idmodule=1;
 }
 
 
@@ -255,29 +266,19 @@ void loop() {
         if (RTC.alarm(ALARM_1))
         {
             digitalWrite(LED, HIGH);
-            Serial.println("");
-            printTime();
             ReadHeSol();
             ReadTempeHeAir();
             delay(50);
+            /*debug
             WriteTempeHeAir();
             WriteHeSol();
+            Serial.println("");
+            printTime();
+            */
             delay(300);
 
     // emission radio de message de relevé de température
-    ++msgradio.nummsg;
-    msgradio.typemsg=1;
-
-
-    Serial.println(F("Now sending"));
-    Serial.print("numéro du msg : ");
-    Serial.println(int(msgradio.nummsg));
-    if (!radio.write( &msgradio, sizeof(msgradio) ))
-    {
-       Serial.println(F("failed"));
-    }  
-       
-    //delay(5000);
+    envoimesure(msgradio);
     }
     alarmIsrWasCalled = false;
     }
